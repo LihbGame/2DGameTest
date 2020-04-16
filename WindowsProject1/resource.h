d0d01a652c2af43705e9062a304ca20e71a6481d
lihongbo <2252895330@qq.com>
2020-04-17 02:04:36 +0800

    add anime for player

---
+++ WindowsProject1/resource.h
@@ -0,0 +1,52 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 WindowsProject1.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_WINDOWSPROJECT1_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WINDOWSPROJECT1             107
+#define IDI_SMALL                       108
+#define IDC_WINDOWSPROJECT1             109
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     129
+#define IDB_BITMAP2                     131
+#define IDB_BITMAP3                     143
+#define IDB_BITMAP4                     144
+#define IDB_BITMAP5                     145
+#define IDB_BITMAP6                     146
+#define IDB_BITMAP7                     147
+#define IDB_BITMAP8                     148
+#define IDB_BITMAP9                     149
+#define IDB_BITMAP10                    150
+#define IDB_BITMAP11                    151
+#define IDB_BITMAP12                    152
+#define IDB_BITMAP13                    153
+#define IDB_BITMAP14                    154
+#define IDB_BITMAP15                    155
+#define IDB_BITMAP16                    156
+#define IDB_BITMAP17                    157
+#define IDB_BITMAP18                    158
+#define IDB_BITMAP19                    159
+#define IDB_BITMAP20                    160
+#define IDB_BITMAP21                    161
+#define IDB_BITMAP22                    162
+#define IDB_BITMAP23                    163
+#define IDB_BITMAP24                    164
+#define IDB_BITMAP25                    165
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        166
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
